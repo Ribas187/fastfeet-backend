2ea346d034e7fabfdab8b638163a0e75b0946fc7
Ribas187 <guilherme.ribas.tech@gmail.com>
2020-02-15 15:34:56 -0300

    env

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -9,7 +9,7 @@ class File extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `${process.env.APP_URL}/files/${this.path}`;
+            return `http://localhost:3330/files/${this.path}`;
           },
         },
       },
